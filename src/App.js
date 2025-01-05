1ca00b4be81b83d9d6387ff91634ef83979c2e55
WickTheThird <filip.bumbu2@mail.dcu.ie>
2025-01-05 13:47:33 +0000

    initializations

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Generate from './components/Generator';
+
+function App() {
+  return (
+    <div>
+      <Generate />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Generator.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const Generate = () => {
+
+  return (<div></div>);
+
+};
+
+export default Generate;
